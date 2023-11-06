dc3d5aa77a23b10d3c2487ac1b5b63c6b8cfb73f
Mallorie Mackay <malloriem123@gmail.com>
2023-11-06 00:02:10 -0700

    Pushing to github so I can't lose it b4 interview

---
+++ src/Board.java
@@ -0,0 +1,131 @@
+/*
+ * Class: Board
+ * Author: Mallorie Mackay
+ * Purpose: Contains the array with information for the board, has funtions to print the board and place Xs and Os
+ */
+
+
+public class Board {
+	private static final int BOARD_SIZE = 3; // set board size variable
+	private char[][] board; //double array holds the board information
+
+	public Board() {
+		board = new char[BOARD_SIZE][BOARD_SIZE];
+	}
+
+	//complete players's turn. if player's choice is already taken, return false. Handle a wrong choice in the tic tac toe game class.
+	public boolean takeTurn(char choice, char player) {
+		switch(choice) {
+		case '1':
+			if(board[0][0]=='X' || board[0][0]=='O') return false; //if the space has been selected, return false
+			board[0][0] = player; //if not, place an X
+			break;
+		case '2':
+			if(board[0][1]=='X' || board[0][1]=='O') return false;
+			board[0][1] = player;
+			break;
+		case '3':
+			if(board[0][2]=='X' || board[0][2]=='O') return false;
+			board[0][2] = player;
+			break;
+		case '4':
+			if(board[1][0]=='X' || board[1][0]=='O') return false;
+			board[1][0] = player;
+			break;
+		case '5':
+			if(board[1][1]=='X' || board[1][1]=='O') return false;
+			board[1][1] = player;
+			break;
+		case '6':
+			if(board[1][2]=='X' || board[1][2]=='O') return false;
+			board[1][2] = player;
+			break;
+		case '7':
+			if(board[2][0]=='X' || board[2][0]=='O') return false;
+			board[2][0] = player;
+			break;
+		case '8':
+			if(board[2][1]=='X' || board[2][1]=='O') return false;
+			board[2][1] = player;
+			break;
+		case '9':
+			if(board[2][2]=='X' || board[2][2]=='O') return false;
+			board[2][2] = player;
+			break;
+		default: //if none of those, invalid move
+			return false;
+		}
+		return true;
+	}
+	
+	//check for tie will be called after each round
+	public boolean checkForTie() { //doesnt need to know player to check a tie
+		for(char[] row: board) {
+			for(char c: row) {
+				if(c!='X' && c!='O') { // if the tile is NOT taken, it cant be a tie
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	//check for win will be called in the game, and it will call methods for each possible win
+	public boolean checkForWin(char player) {
+		if(checkVertical(player)) return true;
+		if(checkHorizontal(player)) return true;
+		if(checkDiagonal(player)) return true;
+		return false;
+	}
+	
+	//****************METHODS TO CHECK FOR WINS*********************\\
+	//all private since only board needs them
+	private boolean checkVertical(char player) { //takes in the last player as only the most recent move can cause a win
+		for(int col=0; col<BOARD_SIZE; col++) { //go through each column
+			if(board[0][col] == player && board[1][col] == player && board[2][col] == player) { //if a column has a win
+				return true;
+			}
+		}
+		return false; //if no win is found, return false
+	}
+
+	private boolean checkHorizontal(char player) { //takes in the last player as only the most recent move can cause a win
+		for(int row=0; row<BOARD_SIZE; row++) { //go through each column
+			if(board[row][0] == player && board[row][1] == player && board[row][2] == player) { //if a row has a win
+				return true;
+			}
+		}
+		return false; //if no win is found, return false
+	}
+
+	private boolean checkDiagonal(char player) {
+		if(board[0][0] == player && board[1][1] == player && board[2][2] == player) { //check for win on diagonal from top left
+			return true;
+		}
+		if(board[0][2] == player && board[1][1] == player && board[2][0] == player) { //check for win on diagonal from top right
+			return true;
+		}
+		return false;
+	}
+
+	//reset function if user wants to play again
+	public void setUp() {
+		char num = '1'; //to be placed in each square, counting up
+		for(int row=0; row<BOARD_SIZE; row++) {
+			for(int col=0; col<BOARD_SIZE; col++) {
+				board[row][col] = num;
+				num++;
+			}
+		}
+	}
+	
+	//Print out the board in a way the user will easily understand
+	public void printBoard() {
+		System.out.println();
+		System.out.printf("  %c | %c | %c%n", board[0][0], board[0][1], board[0][2]); //first row
+		System.out.println(" -----------"); //break for board layout
+		System.out.printf("  %c | %c | %c %n", board[1][0], board[1][1], board[1][2]); //second row
+		System.out.println(" -----------"); //break for board layout
+		System.out.printf("  %c | %c | %c%n", board[2][0], board[2][1], board[2][2]); //third row
+		System.out.println();
+	}
+}
---
+++ src/TicTacToe.java
@@ -0,0 +1,82 @@
+/*
+ * Class: TicTacToe
+ * Author: Mallorie Mackay
+ * Purpose: Plays the game, having its own class allows for MEGA TICTACTOE (which I may implement if i become bored)
+ */
+import java.util.Scanner;
+
+public class TicTacToe {
+	private Board board;
+
+	public TicTacToe() {
+		board = new Board();
+	}
+
+	//method playGame will start the game
+	public void playGame() {
+		board.setUp(); //set up the board to play
+		//begin prompting the player for info
+		System.out.println("Welcome to TicTacToe!\nPlayer X will go first.");
+		board.printBoard(); //print the board so the players can view it
+		//loop until there is a WINNER!!!!
+		boolean win = false;
+		int round = 1; // start at first round, determine player based on round
+		while(!win) {
+			if(round%2 == 1) { //if the round is odd, its x's turn!
+				startRound('X'); //make player x choose spot
+				win = board.checkForWin('X'); //check for a win!
+				if(win) { //if x wins tell them and end game
+					System.out.println("PLAYER X WINS!!!!!!!!");
+					return;
+				}
+			} else {
+				startRound('O');
+				win = board.checkForWin('O'); //check for a win!
+				if(win) { //if x wins tell them and end game
+					System.out.println("PLAYER O WINS!!!!!!!!");
+					return;
+				}
+			}
+			if(board.checkForTie()) { //if theres a tie
+				System.out.println("You tied! Well played :)");
+				return;
+			}
+			round++;
+		}
+	}
+	
+	//method prompts player for choice and takes a turn
+	public void startRound(char player) {
+		System.out.printf("Pick a spot Player %c! (1-9, only the first digit of input will be counted)%n", player);
+		Scanner kb = new Scanner(System.in);
+		char choice = kb.next().charAt(0);
+		kb.nextLine();
+		boolean validChoice = board.takeTurn(choice, player); //take a turn
+		while(!validChoice) { //if the user choice is already selected continue to prompt until they select a valid choice
+			System.out.println("Uh Oh! You can't play there! Pick another spot!");
+			System.out.printf("Pick a spot Player %c! (1-9, only the first digit of input will be counted)%n", player);
+			choice = kb.next().charAt(0);
+			kb.nextLine();
+			validChoice = board.takeTurn(choice, player);
+		}
+		board.printBoard();
+	}
+	
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
+		boolean play = true; //play will keep track of if another game should be played or not
+		while(play) {
+			game.playGame(); //play the game!
+			//when done, see if they'd like to play again!
+			System.out.println("\nWould you like to play again? (\"y\" to continue, any other key to quit)");
+			Scanner kb = new Scanner(System.in);
+			char choice = Character.toUpperCase(kb.next().charAt(0));
+			if(choice == 'Y') {
+				System.out.println("Starting a new game....\n\n");
+			} else {
+				System.out.println("Thanks for playing, goodbye!");
+				play = false;
+			}
+		}
+	}
+}
